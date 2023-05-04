c8a0c8b593633ceae063664a67d920251b094531
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2023-05-04 03:25:06 +0000

    Update main-en.py

--- main-en.py
+++ main-en.py
@@ -1,32 +1,36 @@
-#import the needed modules
+# import the needed modules
 import cv2
 import numpy as np
-from threading import *
+from threading import Thread
 from ultralytics import YOLO
 
-#initialize all variables
-def init_variables():
-    global model, train_new_yolo, face_count,training, epochs, cap
 
-    #get Yolov8 face model
+# initialize all variables
+def init_variables():
+    # get Yolov8 face model
     model = YOLO('train/yolov8n-face.pt')
 
-    #initialize threading
+    # initialize threading
     train_new_yolo = Thread(target=train_it)
 
-    #initalize face count
+    # initalize face count
     face_count = 0
 
-    #initializing variable training
+    # initializing variable training
     training = False
 
-    #initialize epochs
+    # initialize epochs
     epochs = 5
 
-    #initialize camera
+    # initialize camera
     cap = cv2.VideoCapture(0)
 
-#training function
+    return model, train_new_yolo, face_count, training, epochs, cap
+
+
+
+
+# training function
 def train_it():
     global model, training
 
@@ -34,78 +38,79 @@ def train_it():
 
     training = False
 
-#rercord frames and labels + start training process function
+    return 0/0
+
+
+# rercord frames and labels + start training process function
 def record_faces(frame, result):
     global face_count, training, model
 
-    #if training is not started:
+    # if training is not started:
     if not training:
-        #then, see if the number datas are collected enough):
+        # then, see if the number datas are collected enough):
         if face_count < 300:
-            #if number of data is not enough, add more
+            # if number of data is not enough, add more
             face_count += 1
         else:
-            #if number of data is enough, start training thread
+            # if number of data is enough, start training thread
             train_new_yolo.start()
 
-            #set training to true
+            # set training to true
             training = True
 
-            #set face_count to 0
+            # set face_count to 0
             face_count = 0
-            
-        #get the bboxes of the result
+
+        # get the bboxes of the result
         boxes = result[0].boxes
-        
-        #see if the index of data recorded is in the interval between 0 and 99:
+
+        # see if the index of data recorded is in the interval between 0 and 99:
         if face_count >= 0 and face_count < 100:
-            #if the index of data is in the interval between 0 and 99
+            # if the index of data is in the interval between 0 and 99
             filepath = f'train/dataset/images/val/{face_count}.png'
             labelspath = f'train/dataset/labels/val/{face_count}.txt'
         else:
-            #else
+            # else
             filepath = f'train/dataset/images/train/{face_count}.png'
             labelspath = f'train/dataset/labels/train/{face_count}.txt'
-            
+
         cv2.imwrite(filepath, frame)
-        
+
         with open(labelspath, 'w') as file:
             for bboxes in boxes:
                 for xywh in bboxes.xywh:
-                    xywh = xywh * np.array([1/frame.shape[1], 1/frame.shape[0], 1/frame.shape[1], 1/frame.shape[0]])
-                    #write in format of yolo_txt (normalized x and y (center of bbox) and w and h (width and height of the bbox)
+                    xywh = xywh * np.array(
+                        [1 / frame.shape[1], 1 / frame.shape[0], 1 / frame.shape[1], 1 / frame.shape[0]])
+                    # write in format of yolo_txt (normalized x and y (center of bbox) and w and h (width and height of the bbox)
                     file.write(f'0 {xywh[0]} {xywh[1]} {xywh[2]} {xywh[3]}\n')
             file.close()
 
-def main():
-    #call init_variables function to initialize variables
-    init_variables()
-    
-    while True:
 
-        #capture frames in real-time
-        ret, frame = cap.read()
+# call init_variables function to initialize variables
+model, train_new_yolo, face_count, training, epochs, cap=init_variables()
+while True:
 
-        #get results from predicting frame.
-        results = model.predict(frame)
+    # capture frames in real-time
+    ret, frame = cap.read()
 
-        #call function record_faces to record the frame and labels on it.
-        record_faces(frame, results)
+    # get results from predicting frame.
+    results = model.predict(frame)
 
-        #plot the bboxes
-        annotated = results[0].plot()
+    # call function record_faces to record the frame and labels on it.
+    record_faces(frame, results)
 
-        #show to annotated frame
-        cv2.imshow('returned', annotated)
+    # plot the bboxes
+    annotated = results[0].plot()
 
-        #quit sensor
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+    # show to annotated frame
+    cv2.imshow('returned', annotated)
 
-            break
+    # quit sensor
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    cap.release()
+cap.release()
 
-    cv2.destroyAllWindows()
+cv2.destroyAllWindows()
 
-if __name__ == "__main__":
-    main()
+
