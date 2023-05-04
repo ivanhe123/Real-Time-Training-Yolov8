df02ae76251c54ec64e8fbfb8c574ba57dd2ff1b
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2023-05-04 03:25:37 +0000

    Update main-zh.py

--- main-zh.py
+++ main-zh.py
@@ -4,42 +4,47 @@ from threading import Thread
 import numpy as np
 from ultralytics import YOLO
 
+
 # 初始化变量
 def init_variables():
-    global model, train_new_yolo, face_count,training, epochs, cap
-    
+    global model, train_new_yolo, face_count, training, epochs, cap
+
     # 导入人脸检测模型
     model = YOLO('train/yolov8n-face.pt')
-    
+
     # 初始化同步运行训练模块
     train_new_yolo = Thread(target=train_it)
-    
+
     # 初始化人脸录取数变量
     face_count = 0
-    
+
     # 初始化是否在训练中变量
     training = False
-    
+
     # 初始化 epochs
     epochs = 5
-    
+
     # 初始化摄像头
     cap = cv2.VideoCapture(0)
+
 
 # 训练函数
 def train_it():
     global model, training
-    
+
     # 训练模型
     model.train(data='train/custom_data.yaml', epochs=epochs)
-    
+
     # 将在训练设为 False（不在训练）
     training = False
+
+    return 0/0
+
 
 # 记录人脸函数
 def record_faces(frame, result):
     global face_count, training, model
-    
+
     # 检测是否在训练
     if not training:
         # 检测是否需要继续记录人脸
@@ -47,11 +52,10 @@ def record_faces(frame, result):
             face_count += 1
         else:
             # 开始训练新模型
-            train_new_yolo.start()
-            
+
             # 将在训练设为 True（在训练）
             training = True
-            
+
             # 重置人脸录取次数
             face_count = 0
 
@@ -75,36 +79,40 @@ def record_faces(frame, result):
         with open(labelspath, 'w') as file:
             for bboxes in boxes:
                 for xywh in bboxes.xywh:
-                    xywh = xywh * np.array([1/frame.shape[1], 1/frame.shape[0], 1/frame.shape[1], 1/frame.shape[0]])
+                    xywh = xywh * np.array(
+                        [1 / frame.shape[1], 1 / frame.shape[0], 1 / frame.shape[1], 1 / frame.shape[0]])
                     file.write(f'0 {xywh[0]} {xywh[1]} {xywh[2]} {xywh[3]}\n')
+
 
 # 主函数
 def main():
     init_variables()
-    
+    train_new_yolo.start()
+
     while True:
         # 读取实时图像
         ret, frame = cap.read()
-        
+
         # 获取检测的结果
         results = model.predict(frame)
-        
+
         # 记录人脸
         record_faces(frame, results)
-        
+
         # 在图像上绘制检测结果
         annotated = results[0].plot()
-        
+
         # 显示画出来的效果
         cv2.imshow('returned', annotated)
-        
+
         # 检测是否要退出
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
+
     # 释放摄像头并关闭窗口
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
